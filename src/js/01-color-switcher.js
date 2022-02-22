0a40e6e9fc9c561a560f1e700f793c6102ec5593
yakovleva2904 <yakovlevaolga2904@gmail.com>
2022-02-23 00:20:26 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+let intervalId = null;
+let btnDisabled = false;
 
+const refs = {
+    body: document.querySelector('body'),
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+};
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+refs.btnStop.addEventListener('click', onBtnStopClick);
+
+switchDisabledBtns();
+
+function onBtnStartClick() {
+    switchBodyBgColor();
+    intervalId = setInterval(switchBodyBgColor, 1000);
+    btnDisabled = true;
+    switchDisabledBtns()
+ }
+
+function onBtnStopClick() {
+    clearInterval(intervalId);
+    btnDisabled = false;
+    switchDisabledBtns();
+}
+
+function switchDisabledBtns() {    
+    refs.btnStart.disabled = btnDisabled;
+    refs.btnStop.disabled = !btnDisabled;    
+}
+
+function switchBodyBgColor() {
+    const bodyBgColor = getRandomHexColor()
+    refs.body.style.backgroundColor = bodyBgColor;
+} 
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
