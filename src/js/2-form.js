c13a9ab01f696b9beece9df4f84aac04fb99327f
yakovleva2904 <yakovlevaolga2904@gmail.com>
2024-08-27 20:23:28 +0200

    second task is done

---
+++ src/js/2-form.js
@@ -0,0 +1,43 @@
+let formData = {
+  email: '',
+  message: '',
+};
+const form = document.querySelector('.feedback-form');
+
+const fillFormFields = () => {
+    const formDataFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (formDataFromLS === null) {
+        return;
+    }
+
+    formData = formDataFromLS;
+
+    for (const key in formDataFromLS) {
+        if (formDataFromLS.hasOwnProperty(key)) {
+            form.elements[key].value = formDataFromLS[key];
+        }
+    }
+}
+
+fillFormFields();
+
+const inputHandler = e => {
+    formData[e.target.name] = e.target.value.trim();
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const submitHandler = e => {
+    e.preventDefault();
+    if (!formData.email || !formData.message) {
+        alert('Fill please all fields');
+        return;
+    }
+    console.log(formData);
+    e.target.reset();
+    localStorage.removeItem('feedback-form-state');
+    
+};
+
+form.addEventListener('input', inputHandler);
+form.addEventListener('submit', submitHandler);
